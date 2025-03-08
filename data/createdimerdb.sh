72e688d6d0ad8d5179c9e43782c21333b47b7383
SooyoungCha <ellen2g77@gmail.com>
2025-03-09 02:18:41 +0900

    changed awk to cpp

--- data/createdimerdb.sh
+++ data/createdimerdb.sh
@@ -1,130 +1,58 @@
 #!/bin/sh -e
 
-
-#1. sharing monomers by changing lookup files to be possible to have redundant first column -> failed while expandmultimer & scoremultimer
-#2. sharing monomers by changing index files to share second&third columns -> it reduces db memory , but it doesn't reduce computation time
-
-# get list of each chain's id of multimers in chainidxlist (remove monomer chainids)
-# get dimer's original chainid and name in ${OUT}.lookuptmp
-
-exists() {
-	[ -f "$1" ]
+changeIndex() {
+    awk 'FNR==NR{
+        chain_len[$1] = $3; next
+    } BEGIN{
+        i=0
+        d=0
+    } {
+        print d"\t"i"\t"chain_len[$1]
+        i += chain_len[$1]
+        d++
+    }' "${1}.index" "${2}.index" > "${3}.index"
 }
 
 if [ -e "${IN}.dbtype" ]; then
-    awk 'FNR==NR{
-        name[$1]=1; next
-    } {
-        if ($1 in name) {
-            print $0
-        }
-    }' "${IN}.index" "${IN}.lookup" > "${TMP_PATH}/sublookup"
-
-    awk -v firstout="${TMP_PATH}/lookuptmp" 'BEGIN {prev = -1; mon = 0; idx = -1; names = -1} {
-        if(!($3 == prev)) {
-            if (mon > 1) {
-                split(idx, words, " ")
-                split(names, namesplit, " ")
-                for (i = 1; i <= mon - 1; i++) {
-                    print words[i]
-                    for (j = i + 1; j <= mon; j++) {
-                        print words[i]"\t"namesplit[i] >> firstout
-                        print words[j]"\t"namesplit[j] >> firstout
-                    }
-                }
-                print words[mon]
-            }
-            prev = $3
-            mon = 1
-            idx = $1
-            names = $2
-        } else {
-            mon ++
-            idx = idx" "$1
-            names = names" "$2
-        }
-    } END {
-        if (mon > 1) {
-            split(idx, words, " ")
-            split(names, namesplit, " ")
-            for (i = 1; i <= mon - 1; i++) {
-                print words[i]
-                for (j = i + 1; j <= mon; j++) {
-                    print words[i]"\t"namesplit[i] >> firstout
-                    print words[j]"\t"namesplit[j] >> firstout
-                }
-            }
-            print words[mon]
-        }
-    }' "${TMP_PATH}/sublookup" > "${TMP_PATH}/chainidxlist"
-
-    awk -v secondout="${TMP_PATH}/reallookup" '{
-        print NR-1"\t"$2"\t"int((NR-1)/2) >> secondout
-        print NR-1"\t"$1
-    }' "${TMP_PATH}/lookuptmp" > "${TMP_PATH}/realIdx_originalIdx"
-
-    awk '{print $3"\t"$2}' "${TMP_PATH}/reallookup" | awk -F'_' 'NR%2==1{OFS="_"; $NF=""; sub(/_$/, "", $0); print $0}' > "${TMP_PATH}/realsource" 
-
     # shellcheck disable=SC2086
-    "$MMSEQS" createsubdb "${TMP_PATH}/chainidxlist" "${IN}" "${TMP_PATH}/out" --subdb-mode 0 ${VERBOSITY_PAR} \
-        || fail "createsubdb died"
-
-    if exists "${TMP_PATH}/out.lookup" ; then
-        rm -- "${TMP_PATH}/out.lookup"
-    fi
-    if exists "${TMP_PATH}/out.source" ; then
-        rm -- "${TMP_PATH}/out.source"
-    fi
-    
-    # shellcheck disable=SC2086
-    "$MMSEQS" base:createsubdb "${TMP_PATH}/chainidxlist" "${IN}_h" "${TMP_PATH}/out_h" --subdb-mode 0 ${VERBOSITY_PAR} \
-        || fail "createsubdb died"
-    mv "${TMP_PATH}/reallookup" "${TMP_PATH}/dimerdb.lookup"
-    mv "${TMP_PATH}/realsource" "${TMP_PATH}/dimerdb.source"
-
-    awk 'FNR==NR{name[$1]=$2"\t"$3; next}{print $1"\t"name[$2]}' "${TMP_PATH}/out_h.index" "${TMP_PATH}/realIdx_originalIdx" > "${TMP_PATH}/dimerdb_h.index"
-    awk 'FNR==NR{name[$1]=$2"\t"$3; next}{print $1"\t"name[$2]}' "${TMP_PATH}/out.index" "${TMP_PATH}/realIdx_originalIdx" > "${TMP_PATH}/dimerdb.index"
-    awk 'FNR==NR{name[$1]=$2"\t"$3; next}{print $1"\t"name[$2]}' "${TMP_PATH}/out_ss.index" "${TMP_PATH}/realIdx_originalIdx" > "${TMP_PATH}/dimerdb_ss.index"
-    awk 'FNR==NR{name[$1]=$2"\t"$3; next}{print $1"\t"name[$2]}' "${TMP_PATH}/out_ca.index" "${TMP_PATH}/realIdx_originalIdx" > "${TMP_PATH}/dimerdb_ca.index"
-
-    mv "${TMP_PATH}/out_h" "${TMP_PATH}/dimerdb_h"
-    mv "${TMP_PATH}/out" "${TMP_PATH}/dimerdb"
-    mv "${TMP_PATH}/out_ss" "${TMP_PATH}/dimerdb_ss"
-    mv "${TMP_PATH}/out_ca" "${TMP_PATH}/dimerdb_ca"
-    mv "${TMP_PATH}/out_h.dbtype" "${TMP_PATH}/dimerdb_h.dbtype"
-    mv "${TMP_PATH}/out.dbtype" "${TMP_PATH}/dimerdb.dbtype"
-    mv "${TMP_PATH}/out_ss.dbtype" "${TMP_PATH}/dimerdb_ss.dbtype"
-    mv "${TMP_PATH}/out_ca.dbtype" "${TMP_PATH}/dimerdb_ca.dbtype"
-    # shellcheck disable=SC2086
-    "$MMSEQS" filterdimerdb "${TMP_PATH}/dimerdb" "${TMP_PATH}/contactlist" ${FILTERDIMERDB_PAR} \
+    "$MMSEQS" filterdimerdb "${IN}" "${TMP_PATH}/contactlist" ${FILTERDIMERDB_PAR} \
         || fail "filterdimerdb died"
-    # shellcheck disable=SC2086
-    "$MMSEQS" createsubdb "${TMP_PATH}/contactlist.index" "${TMP_PATH}/dimerdb" "${OUT}" --subdb-mode 0 ${VERBOSITY_PAR} \
-        || fail "createsubdb died"
-    # shellcheck disable=SC2086
-    "$MMSEQS" mvdb "${TMP_PATH}/dimerdb_h" "${OUT}_h"
-    mv "${TMP_PATH}/dimerdb.lookup" "${OUT}.lookup"
-    mv "${TMP_PATH}/dimerdb.source" "${OUT}.source"
+    "$MMSEQS" lndb "${IN}" "${OUT}" \
+        || fail "lndb died"
+    "$MMSEQS" lndb "${IN}_ss" "${OUT}_ss" \
+        || fail "lndb died"
+    "$MMSEQS" lndb "${IN}_ca" "${OUT}_ca" \
+        || fail "lndb died"
+    "$MMSEQS" lndb "${IN}_h" "${OUT}_h" \
+        || fail "lndb died"
 
+    rm "${OUT}.index"
+    rm "${OUT}_ss.index"
+    rm "${OUT}_ca.index"
+    rm "${OUT}_h.index"
+
+    changeIndex "${IN}" "${TMP_PATH}/contactlist" "${OUT}"
+    changeIndex "${IN}_ss" "${TMP_PATH}/contactlist" "${OUT}_ss"
+    changeIndex "${IN}_ca" "${TMP_PATH}/contactlist" "${OUT}_ca"
+    changeIndex "${IN}_h" "${TMP_PATH}/contactlist" "${OUT}_h"
+    
+    rm "${OUT}.lookup"
+    rm "${OUT}.source"
+    
+    awk 'FNR==NR {
+        chainname[$1] = $2; next
+    } BEGIN {i = 0} {
+        print i"\tDI"int(i/2)"_"chainname[$1]"\t"int(i/2)
+        i++
+    }' "${IN}.lookup" "${TMP_PATH}/contactlist.index" > "${OUT}.lookup"
+
+    awk 'NR%2==1 {
+        sub(/_[^_]+$/, "", $2)
+        print $3"\t"$2
+    }' "${OUT}.lookup" > "${OUT}.source"
 fi
 
 if [ -n "${REMOVE_TMP}" ]; then
-    rm "${TMP_PATH}/chainidxlist"
-    rm "${TMP_PATH}/lookuptmp"
-    rm "${TMP_PATH}/realIdx_originalIdx"
-    rm "${TMP_PATH}/out_ca.index"
-    rm "${TMP_PATH}/out_h.index"
-    rm "${TMP_PATH}/out.index"
-    rm "${TMP_PATH}/out_ss.index"
-    # shellcheck disable=SC2086
-    "$MMSEQS" rmdb "${TMP_PATH}/dimerdb"
-    # shellcheck disable=SC2086
-    "$MMSEQS" rmdb "${TMP_PATH}/dimerdb_h"
-    # shellcheck disable=SC2086
-    "$MMSEQS" rmdb "${TMP_PATH}/dimerdb_ca"
-    # shellcheck disable=SC2086
-    "$MMSEQS" rmdb "${TMP_PATH}/dimerdb_ss"
     # shellcheck disable=SC2086
     "$MMSEQS" rmdb "${TMP_PATH}/contactlist"
-    rm -- "${TMP_PATH}/sublookup"
 fi
